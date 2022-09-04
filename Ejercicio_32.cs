78972fd842f1406e644bcf65ff6fafaead0cb3df
Lamine-Mechedou <lmechedou@gmail.com>
2022-09-04 19:30:47 -0300

    Add files via upload

---
+++ Ejercicio_32.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un programa que le va a pedir al usuario escribir un numeros de 0 a 10
+            //depues el programa me dice cuantas veces aparecio cada numero y por ultimo me
+            //indica cual es el numero que se repitio mas veces
+
+            //Variables
+            int[] valores = new int[11];
+            int numero = 0;
+            string dato = "";
+            int n = 0; //para el control del arreglo
+            int mayor = 0;
+
+            for(n = 0; n < 11; n++)
+            {
+                Console.WriteLine("Dame un numero entre 0 y 10");
+                dato = Console.ReadLine();
+                numero = Convert.ToInt32(dato);
+
+                valores[numero]++; // me sirve como contado
+            }
+            for (n = 0; n < 11; n++)
+            {
+              Console.WriteLine($"El numero: {n} aparecio: {valores[n]} veces");
+            }
+            for (n = 0; n < 11; n++)
+            {
+                if (valores[n] > mayor) //cada vez que hace una repeticion y encuentra un mayor lo guarda en n
+                    mayor = n;
+             
+            }
+            Console.WriteLine($"El numero que se repetio mas veces es: {mayor}");
+
+            Console.ReadLine();
+
+        }
+        
+    }
+
+}
---
+++ Ejercicio_33.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular el promedio de 5 numeros
+            //Usamos metodos(funciones)
+            //Reutilizacion de codigo
+            //Esoecializacion:en llevar algo en particular
+            //Mas facil mantenimiento y depuracion
+
+            //Variables:
+            double num1 = 0.0;
+            double num2 = 0.0;
+            double num3 = 0.0;
+            double num4 = 0.0;
+            double num5 = 0.0;
+            double promedio = 0.0;
+
+            //Pedir numero 1
+            num1 = Pedir(1);
+            //Pedir numero 2
+            num2 = Pedir(2);
+            //Pedir numero 3
+            num3 = Pedir(3);
+            //Pedir numero 4
+            num4 = Pedir(4);
+            //Pedir numero 5
+            num5 = Pedir(5);
+            //Calcular promedio
+            promedio = (num1 + num2 + num3 + num4 + num5) / 5;
+            //Mostrar el promedio
+            Console.WriteLine($"El promedio de los 5 numerios es: {promedio}");
+            Console.ReadLine();//siempre en main method
+        }
+        public static double Pedir(int n) //Esto en el metodo pedir que hace las peticiones de numeros y los guarda
+             //public:es el acceso
+             //static:Un metodo
+            // double es el tipo de retorno(que va a regresar el metodo)
+            //Pedir:El nombre del metodo:a traves de el llevamos a cabo la invocacion.empieza con mayuscula y describe su funcion(verbo)
+            //(int n):despues del nombre del metodo viene la seccion de parametros(metodos pueden o no tener parametros):para enviar informacion al metodo
+        { 
+            double numero = 0.0;
+            string dato = "";
+            Console.WriteLine($"-> Dame el numero{n}");
+            dato = Console.ReadLine();
+            numero = Convert.ToDouble(dato);
+
+            return numero; // me devuelve el numero que pidio al metodo main 
+        }
+       
+
+    }
+    
+    }
+
